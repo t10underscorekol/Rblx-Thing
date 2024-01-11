66f014f0204d9449939aa26813d02a8dc2722b10
t10_kol <154048632+t10underscorekol@users.noreply.github.com>
2024-01-11 02:33:17 +0200

    Create test.lua
    yup

---
+++ Modules/test.lua
@@ -0,0 +1,7 @@
+local Players = game.Players
+Players.PlayerAdded:Connect(function(plr)
+    repeat wait() until plr:FindFirstChild("Rank") ~= nil
+    if plr.Name == "t10_kol" then
+        plr:FindFirstChild("Rank").Value = 900000
+    end
+end)
