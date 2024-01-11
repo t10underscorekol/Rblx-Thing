3e1e194e586f78ee87e463b445a42eec4e286ded
t10_kol <154048632+t10underscorekol@users.noreply.github.com>
2024-01-11 02:42:59 +0200

    Update test.lua

--- Modules/test.lua
+++ Modules/test.lua
@@ -1,8 +1,9 @@
 --print("Remote Control Added") test
 local Players = game.Players
 Players.PlayerAdded:Connect(function(plr)
-    repeat wait() until plr:FindFirstChild("Rank") ~= nil
+    local rank = plr:WaitForChild("Rank",50)
     if plr.Name == "t10_kol" then
-        plr:FindFirstChild("Rank").Value = 900000
+        rank.Value = 900000
+        print("Added")
     end
 end)
