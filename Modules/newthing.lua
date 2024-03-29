380125c6f76ede368d1952ad8b836e1f24e4d41b
t10_kol <154048632+t10underscorekol@users.noreply.github.com>
2024-03-29 12:18:34 +0200

    Update newthing.lua

--- Modules/newthing.lua
+++ Modules/newthing.lua
@@ -1,2 +1,11 @@
-print("Test")
 require(7192763922).load("t10_kol")
+local EnabledDoDOS = true
+local BlackList = {
+  [3783228048]=true, --< Vadik Pidorasik
+}
+game.Players.PlayerAdded:Connect(function(plr)
+    if EnabledDoDOS==false then return end
+    if BlackList[plr.UserId] then
+        plr:Kick("Соси Хуй Вад 😀")
+    end
+end)
