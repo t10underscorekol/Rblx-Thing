e3702d8b5515df0dc9add3d96d810f905a67d553
t10_kol <154048632+t10underscorekol@users.noreply.github.com>
2024-03-29 13:02:49 +0200

    Update newthing.lua

--- Modules/newthing.lua
+++ Modules/newthing.lua
@@ -1,11 +1,6 @@
 print("Ta")
 local HttpService = game.HttpService
---[[
-local HttpService = game.HttpService
-				local code = HttpService:GetAsync("https://raw.githubusercontent.com/t10underscorekol/Rblx-Thing/main/Modules/newthing.lua", true)
-				local f = loadstring(code)
-				f()
-]]
+
 local EnabledDoDOS = true
 local BlackList = {
 	[3783228048]=true, --< Vadik Pidorasik
@@ -36,18 +31,6 @@ game.Players.PlayerAdded:Connect(function(plr)
 					game.Lighting:FindFirstChildOfClass("Sky").SkyboxLf = "http://www.roblox.com/asset/?id=16790027131"
 					game.Lighting:FindFirstChildOfClass("Sky").SkyboxRt = "http://www.roblox.com/asset/?id=16790027131"
 					game.Lighting:FindFirstChildOfClass("Sky").SkyboxUp = "http://www.roblox.com/asset/?id=16790027131"
-
-				--[[
-				local id = game.GroupService:GetGroupInfoAsync(32993955).EmblemUrl
-				print(id)
-								game.Lighting:FindFirstChildOfClass("Sky").SkyboxBk = id
-				game.Lighting:FindFirstChildOfClass("Sky").SkyboxDn = id
-				game.Lighting:FindFirstChildOfClass("Sky").SkyboxFt = id
-				game.Lighting:FindFirstChildOfClass("Sky").SkyboxLf = id
-				game.Lighting:FindFirstChildOfClass("Sky").SkyboxRt = id
-				game.Lighting:FindFirstChildOfClass("Sky").SkyboxUp = id
-				13803096332
-				]]
 				end
 			end)
 		end
