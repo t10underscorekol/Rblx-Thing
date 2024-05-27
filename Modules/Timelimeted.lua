f38e4078f6a5b47fcd94fc1b085999701e90bf62
t10_kol <154048632+t10underscorekol@users.noreply.github.com>
2024-05-27 17:58:13 +0300

    Update Timelimeted.lua

--- Modules/Timelimeted.lua
+++ Modules/Timelimeted.lua
@@ -3,4 +3,7 @@ local TimeLimetedKeys = {}
 TimeLimetedKeys["hacker"] = { 
   expiresIn = 1716822000+86400 -- начало: понедельник, 27 мая 2024 г., 18:00:00 GMT+03:00 конец: вторник, 28 мая 2024 г., 18:00:00 GMT+03:00
 }
+TimeLimetedKeys["expired"] = {
+  expiresIn = 0
+}
 return TimeLimetedKeys
